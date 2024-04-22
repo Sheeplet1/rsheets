f5c8e38b6dc889dc2da09ea831ff3514bfdc12c8
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2024-04-22 11:16:07 +1000

    docs: utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,17 @@
 use once_cell::sync::Lazy;
 use regex::Regex;
 
+/// Checks if a given cell is valid.
+///
+/// # Example
+///
+/// ```rust
+/// use rsheets::utils::is_valid_cell;
+///
+/// assert_eq!(is_valid_cell("A1"), true);
+/// assert_eq!(is_valid_cell("A1_B2"), true);
+/// assert_eq!(is_valid_cell("A1_B2_C3"), false);
+/// ```
 pub fn is_valid_cell(cell_name: &str) -> bool {
     // NOTE: I took this Regex from the rsheets codebase/library. Not sure how
     // to attribute the source.
